cb35f6e1a16827444d42b69e67b12724e698ae21
Jonathan <valla_jo@hotmail.com>
2019-07-15 08:59:03 +0200

    humble beginnings

---
+++ src/com/hb/appniou/Entry.java
@@ -0,0 +1,13 @@
+package com.hb.appniou;
+
+public class Entry {
+
+	public static void main(String[] args) {
+		String truc = "Bonjour ";
+		String truc2 = "Jo";
+		int age = 33;
+		
+		System.out.println(truc + truc2 + " ! Vous avez " + age + " ans.");
+	}
+
+}
